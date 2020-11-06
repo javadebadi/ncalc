d3361d2e14e1999f18e4a40187a050908e08a829
javadebadi <javad.ebadi.1990@gmail.com>
2020-11-06 14:43:50 +0330

    Update: change stucture of the code and added simple features

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from 'react';
 import "./App.css";
 import NumPad from "./components/NumPad";
 
 
-function App() {
-  return (
-    <div className="App">
-      <NumPad />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+      super(props);
+      this.state={
+        numberInput: null,
+      }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NumPad />
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/components/NumPad.js
+++ src/components/NumPad.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 function NumberSquare(props) {
   return (
     <button className="number-square">
@@ -6,31 +8,46 @@ function NumberSquare(props) {
   );
 }
 
-function NumPad() {
-  return (
-    <div className="numpad">
-      <div className="numpad-row">
-        <NumberSquare value={1} />
-        <NumberSquare value={2} />
-        <NumberSquare value={3} />
+class NumPad extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderNumberSquare(i){
+    if (typeof i  !== "string"){
+      return <NumberSquare value={i}/>;
+    }
+    else {
+      return <NumberSquare value={i}/>;
+    }
+  }
+
+  render() {
+    return (
+      <div className="numpad">
+        <div className="numpad-row">
+          {this.renderNumberSquare(1)}
+          {this.renderNumberSquare(2)}
+          {this.renderNumberSquare(3)}
+        </div>
+        <div className="numpad-row">
+          {this.renderNumberSquare(4)}
+          {this.renderNumberSquare(5)}
+          {this.renderNumberSquare(6)}
+        </div>
+        <div className="numpad-row">
+          {this.renderNumberSquare(7)}
+          {this.renderNumberSquare(8)}
+          {this.renderNumberSquare(9)}
+        </div>
+        <div className="numpad-row">
+          {this.renderNumberSquare("+ -")}
+          {this.renderNumberSquare(0)}
+          {this.renderNumberSquare(".")}
+        </div>
       </div>
-      <div className="numpad-row">
-        <NumberSquare value={4} />
-        <NumberSquare value={5} />
-        <NumberSquare value={6} />
-      </div>
-      <div className="numpad-row">
-        <NumberSquare value={7} />
-        <NumberSquare value={8} />
-        <NumberSquare value={9} />
-      </div>
-      <div className="numpad-row">
-        <NumberSquare value="+ -" />
-        <NumberSquare value={0} />
-        <NumberSquare value="." />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 
